1fcdacef59e840861eeb33e2955ad0a67865b156
vy.ct <vyct@mor.com.vn>
2023-02-13 16:22:22 +0700

    insertionSort

---
+++ sort/insertionSort.js
@@ -0,0 +1,18 @@
+function insertionSort (array) {
+    
+    for (var i = 1; i < array.length; i++) {
+        var current = array[i];
+        var j = i - 1;
+
+        while (array[j] > 0 && array[j] > current) {
+            array[j + 1] = array[j];
+            j--
+        }
+
+        array[j + 1] = current;
+    }
+
+    return array
+}
+
+console.log(insertionSort([37, 45, 29, 8]))
